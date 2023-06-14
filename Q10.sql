ec0fbc5427b3d89d70d43f3115948066c136dfd4
yashr2001 <87497512+yashr2001@users.noreply.github.com>
2023-06-14 15:46:14 +0530

    Delete Q10.sql

--- Q10.sql
+++
@@ -1,7 +0,0 @@
-/* 10. List all ‘Orders’ with ‘Customer’ details and ‘Employee’ who processed it */
-select *
-from orders
-inner join customers
-on orders.customer_id = customers.customer_id
-inner join employees
-on orders.employee_id = employees.employee_id ;
